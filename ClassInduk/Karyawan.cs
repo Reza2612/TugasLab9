79ff4f3e42a08484a9c9988c1337c6e262260267
Reza2612 <61917869+Reza2612@users.noreply.github.com>
2020-06-30 21:37:03 +0700

    Add files via upload

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TugasLab8.ClassInduk
+{
+    abstract class Karyawan
+    {
+        public string Nama { get; set; }
+        
+		public string NIK { get; set; }
+        
+		public abstract double Gaji();
+    }
+}
